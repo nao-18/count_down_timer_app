df09eb8b1cc2715851e9559e981b3473a7230a66
nao18 <justdo.nao01@gmail.com>
2021-05-03 14:39:06 +0900

    シークバー処理追加

--- app/src/main/java/com/example/mycountdowntimer/MainActivity.kt
+++ app/src/main/java/com/example/mycountdowntimer/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.AdapterView
+import android.widget.SeekBar
 import android.widget.Spinner
 import com.example.mycountdowntimer.databinding.ActivityMainBinding
 
@@ -57,6 +58,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+        // シークバー処理
+        binding.seekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {
+                    timer.cancel()
+                    binding.playStop.setImageResource(
+                        R.drawable.ic_baseline_play_arrow_24
+                    )
+                    val min = progress / 60L
+                    val sec = progress % 60L
+                    binding.timerText.text = "%1d:%2$02d".format(min, sec)
+                    timer = MyCountDownTimer(progress * 1000L, 100)
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+            }
+        )
 
         // スピナー処理
         binding.spinner.onItemSelectedListener =
